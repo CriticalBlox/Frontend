ce8c9e332eaafdc69aa8dfb9783c4521ab5f15f9
Kilian <kvanovertvelt@gmail.com>
2026-05-19 18:53:06 +0200

    feat: footer

--- Critical_blox/src/views/HomeView.vue
+++ Critical_blox/src/views/HomeView.vue
@@ -4,6 +4,7 @@ import NavBar from '@/components/NavBar.vue'
 import { useAuthStore } from '@/stores/authStore'
 import Home from "@/components/Home.vue";
 import InfoSection from "@/components/InfoSection.vue";
+import Footer from "@/components/Footer.vue";
 
 const authStore = useAuthStore()
 
@@ -16,6 +17,7 @@ onMounted(() => {
   <NavBar />
   <Home/>
   <InfoSection/>
+  <Footer/>
 
   <p v-if="authStore.user">
     {{ authStore.user.email }}
