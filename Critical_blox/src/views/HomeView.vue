77754d2db094b17bb428171b7665571eeca19530
Kilian <kvanovertvelt@gmail.com>
2026-05-19 11:57:16 +0200

    feat: initial commit

---
+++ Critical_blox/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { HomeIcon } from '@heroicons/vue/24/outline'
+</script>
+
+<template class="bg-white">
+  <h1 class="text-4xl font-bold text-blue-500">
+    Tailwind fonctionne !
+  </h1>
+
+  <button class="btn btn-primary">
+    Bouton DaisyUI
+  </button>
+
+  <HomeIcon class="w-10 h-10 text-blue-500" />
+
+</template>
